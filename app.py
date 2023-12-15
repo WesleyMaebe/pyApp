6847d41780b3ad1bbe870a04d36fda7126faadd4
WesleyMaebe <wesley.maebe@student.hogent.be>
2023-12-15 20:00:37 +0100

    test

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
-def add_numbers(a, b):
+import random
+
+def calculating(a,b):
+  if (a == b):
+     return multiplying(adding(a, b), adding(a,b))
+  if (a > b):   
+     return -10
+  else:
+    return b
+
+def adding(a, b):
     return a + b
+
+def multiplying(a,b):
+   return a * b
+
+for _ in range(5):
+    a = random.randint(1, 10)
+    b = random.randint(1, 5)
+    total_points = calculating(a, b)
+    print(total_points)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,21 @@
-from app import add_numbers
+from app import adding, multiplying, calculating
 
-def test_add_numbers():
-    result = add_numbers(2, 3)
-    assert result == 5
+def test_calculating():
+    result = calculating(2, 2)
+    assert result == 16
+
+def test_calculating2():
+    result = calculating(7, 3)
+    assert result == -10
+
+def test_calculating3():
+    result = calculating(1, 4)
+    assert result == 4
+  
+def test_multiplying():
+    result = multiplying(2, 5)
+    assert result == 10
+
+def test_adding():
+    result = adding(6, 3)
+    assert result == 9
